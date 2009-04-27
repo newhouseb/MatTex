04a1b3ebc6f1c077ac9bf0361389e762ac0e5e53
Ben Newhouse <newhouseb@gmail.com>
2009-04-27 03:14:03 -0700

    added a catch block around the extraced matlab

--- mattex.sh
+++ mattex.sh
@@ -2,14 +2,16 @@
 
 #Scrape out the MATLAB, prepare it to be run
 name=`echo "$1" | perl -pe 's/\.tex//'`
-perl -0777 -ne 'undef $/; print for m/<\?ml.*?>/msg' "$1" | perl -pe 's/<\?ml/thisisalinebreak=1/g; s/\?>//g;' > "$name.m"
+echo "try" > "$name.m"
+perl -0777 -ne 'undef $/; print for m/<\?ml.*?>/msg' "$1" | perl -pe 's/<\?ml/thisisalinebreak=1/g; s/\?>//g;' >> "$name.m"
 echo "thisisalinebreak=1" >> "$name.m"
 echo "exit" >> "$name.m"
+echo "catch ME" >> "$name.m"
+echo "exit" >> "$name.m"
+echo "end" >> "$name.m"
 
 #Run matlab, cat output to a file
 matlab -r "$name" > "$name.out"
-
-cat "$name.out"
 
 ./mattex.py "$name.out" "$name.tex" > "$name.parsed.tex"
 
